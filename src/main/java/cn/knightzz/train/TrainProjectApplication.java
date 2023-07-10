aec9637c08782607f1848219034c5708d878d3dc
王天赐 <15565946702@163.com>
2023-07-10 17:00:25 +0800

    项目初始化

---
+++ src/main/java/cn/knightzz/train/TrainProjectApplication.java
@@ -0,0 +1,16 @@
+package cn.knightzz.train;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 王天赐
+ */
+@SpringBootApplication
+public class TrainProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/knightzz/train/TrainProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.knightzz.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
