146e868f45c084115cf1905cc90ac0a21ddd16e8
王天赐 <15565946702@163.com>
2023-07-10 20:58:44 +0800

    修改启动日志, 添加Banner以及日志配置文件

--- train-member/src/main/java/cn/knightzz/train/member/member/config/TrainMemberApplication.java
+++ train-member/src/main/java/cn/knightzz/train/member/member/config/TrainMemberApplication.java
@@ -1,8 +1,11 @@
 package cn.knightzz.train.member.member.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
 
 /**
  * @author 王天赐
@@ -11,8 +14,12 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan("cn.knightzz")
 public class TrainMemberApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TrainMemberApplication.class, args);
-    }
+    private static final Logger LOG = LoggerFactory.getLogger(TrainMemberApplication.class);
 
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(TrainMemberApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
 }
---
+++ train-member/src/main/java/cn/knightzz/train/member/member/controller/HelloController.java
@@ -0,0 +1,22 @@
+package cn.knightzz.train.member.member.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 王天赐
+ * @title: HelloController
+ * @projectName train-project
+ * @description:
+ * @website <a href="https://knightzz.cn/">https://knightzz.cn/</a>
+ * @github <a href="https://github.com/knightzz1998">https://github.com/knightzz1998</a>
+ * @create: 2023-07-10 20:42
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
